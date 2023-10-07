fc0c964028746db03e63078ad46d2481f23be259
Puyed <amrut-rajesh.puyed@capgemini.com>
2023-10-08 01:29:00 +0530

    changed few things

---
+++ src/components/SurfEbooks/DownloadPage.js
@@ -0,0 +1,20 @@
+// DownloadPage.js
+import React from 'react';
+
+function DownloadPage({ subscriptionCardData }) {
+  return (
+    <div>
+      {/* First Card */}
+      <div className='card' style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' }}>
+        {/* ... Card content similar to the preview page ... */}
+      </div>
+
+      {/* Second Card */}
+      <div className='card'>
+        {/* ... Card content similar to the preview page ... */}
+      </div>
+    </div>
+  );
+}
+
+export default DownloadPage;
